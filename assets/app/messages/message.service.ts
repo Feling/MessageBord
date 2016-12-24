7752dc7fa06a6a458b8648f480e9c504ec64e684
igork <feling87@gmail.com>
2016-12-24 13:04:12 +0200

    Delete message function

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -43,7 +43,7 @@ export class MessageService {
     }
 
     updateMessage(message: Message) {
-        const body = JSON.stringify(message)
+        const body = JSON.stringify(message);
         const headers = new Headers({'Content-Type': 'application/json'});
         return this.http.patch('http://localhost:3000/message/' + message.messageId, body, {headers: headers})
             .map((response: Response) => response.json())
@@ -52,5 +52,8 @@ export class MessageService {
 
     deleteMessage(message: Message) {
         this.messages.splice(this.messages.indexOf(message), 1);
+        return this.http.delete('http://localhost:3000/message/' + message.messageId)
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
     }
 }
