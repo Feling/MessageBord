e3cf216d024a9830966f4efa44c5e2c4fef3ab0f
igork <feling87@gmail.com>
2016-12-24 21:15:56 +0200

    Adding new User service

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,15 +1,18 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {AuthService} from "./auth.service";
 
 @Component({
     selector: 'app-signin',
     templateUrl: './signin.component.html'
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit {
     myForm: FormGroup;
 
+    constructor(private authService: AuthService) {
+    }
+
     onSubmit() {
-        console.log(this.myForm);
         this.myForm.reset();
     }
 
