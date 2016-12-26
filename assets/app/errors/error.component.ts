b3b28f9d58e590e155e28bc1aa4e585574c1a01d
igork <feling87@gmail.com>
2016-12-26 17:32:18 +0200

    Html Error handling wrap

--- assets/app/errors/error.component.ts
+++ assets/app/errors/error.component.ts
@@ -1,5 +1,7 @@
-import {Component} from "@angular/core";
+///<reference path="../../../node_modules/@angular/core/src/metadata/lifecycle_hooks.d.ts"/>
+import {Component, OnInit} from "@angular/core";
 import {Error} from './error.model';
+import {ErrorService} from "./error.service";
 
 @Component({
     selector: 'app-error',
@@ -15,11 +17,26 @@ import {Error} from './error.model';
     }
     `]
 })
-export class ErrorComponent {
+export class ErrorComponent implements OnInit {
+
     error: Error;
-    display: 'none';
+    display = 'none';
+
+    constructor(private errorService: ErrorService) {
+    }
 
     onErrorHandled() {
         this.display = 'none';
     }
+
+    ngOnInit() {
+        this.errorService.errorOccurred
+            .subscribe(
+                (error: Error) => {
+                    this.error = error;
+                    this.display = 'block';
+                }
+            );
+    }
+
 }
