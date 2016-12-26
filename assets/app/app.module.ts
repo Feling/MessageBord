394fcee6e33dac759c0d7f87fb9ec8827fd3fe69
igork <feling87@gmail.com>
2016-12-26 17:52:46 +0200

    Module splitting

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,12 +1,8 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
 
 import {AppComponent} from "./app.component";
-import {MessageComponent} from "./messages/message.component";
-import {MessageListComponent} from "./messages/message-list.component";
-import {MessageInputComponent} from "./messages/message-input.component";
-import {MessagesComponent} from "./messages/messages.component";
 import {AuthenticationComponent} from "./auth/authentication.component";
 import {HeaderComponent} from "./header.component";
 import {routing} from "./app.routing";
@@ -17,14 +13,11 @@ import {HttpModule} from "@angular/http";
 import {AuthService} from "./auth/auth.service";
 import {ErrorComponent} from "./errors/error.component";
 import {ErrorService} from "./errors/error.service";
+import {MessageModule} from "./messages/message.module";
 
 @NgModule({
     declarations: [
         AppComponent,
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
         LogoutComponent,
@@ -32,7 +25,7 @@ import {ErrorService} from "./errors/error.service";
         SigninComponent,
         ErrorComponent
     ],
-    imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule, HttpModule],
+    imports: [BrowserModule, routing, ReactiveFormsModule, HttpModule, MessageModule],
     providers: [AuthService, ErrorService],
     bootstrap: [AppComponent]
 })
