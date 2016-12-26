5ab32d1860b2ebf3a3a5e426f60c6318b0f5888c
igork <feling87@gmail.com>
2016-12-26 16:17:17 +0200

    belongsTo check

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -37,4 +37,9 @@ export class MessageComponent {
                 result => console.log(result)
             );
     }
+
+    belongsToUser() {
+        return localStorage.getItem('userId') == this.message.userId;
+    }
+
 }
